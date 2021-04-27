5659370eab85a37c8a4ebf798978dc4f8111bc75
Karen Alicia Hernandez <karenaliciahernandez@hotmail.com>
2021-04-27 17:51:21 -0600

    Coverage 100%

--- src/test/java/mx/tec/lab/OddAndEvenEvaluatorTest.java
+++ src/test/java/mx/tec/lab/OddAndEvenEvaluatorTest.java
@@ -17,4 +17,26 @@ public class OddAndEvenEvaluatorTest {
 		// Then
 		assertEquals("C is even and D is even, c=4.0 and d=0.0", result);
 	}
+	
+	@Test
+	public void givenA3B1WhenEvaluateThenCEvenDEven() {
+		// Given
+		double a = 3;
+		double b = 1;
+		// When
+		String result = evaluator.evaluate(a, b);
+		// Then
+		assertEquals("C is even and D is even, c=4.0 and d=2.0", result);
+	}
+	
+	@Test
+	public void givenA3B2WhenEvaluateThenCOddDOdd() {
+		// Given
+		double a = 3;
+		double b = 2;
+		// When
+		String result = evaluator.evaluate(a, b);
+		// Then
+		assertEquals("C is odd and D is odd, c=5.0 and d=1.0", result);
+	}
 }
